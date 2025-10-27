7a82f1836608b66b29a1da454e59ccae264dfdf1
maybe-j-fischer <10869729+maybe-j-fischer@users.noreply.github.com>
2025-10-27 14:04:32 +0100

    added homework B01

--- src/adap-b01/names/Name.ts
+++ src/adap-b01/names/Name.ts
@@ -19,17 +19,30 @@ export class Name {
     private components: string[] = [];
 
     /** Expects that all Name components are properly masked */
+	// @methodtype initialization-method
     constructor(other: string[], delimiter?: string) {
-        throw new Error("needs implementation or deletion");
+		this.components = other;
+
+		if (delimiter != undefined) {
+			this.delimiter = delimiter;
+		}
     }
+
+	/** Escapes any character that has a special meaning in regex */
+	private static escapeRegexChars(s: string): string {
+		return s.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+	}
 
     /**
      * Returns a human-readable representation of the Name instance using user-set special characters
      * Special characters are not escaped (creating a human-readable string)
      * Users can vary the delimiter character to be used
      */
+	// @methodtype conversion-method
     public asString(delimiter: string = this.delimiter): string {
-        throw new Error("needs implementation or deletion");
+		const regexp_delimiter = new RegExp(Name.escapeRegexChars(ESCAPE_CHARACTER + this.delimiter), "g");
+		const regexp_escape = new RegExp(Name.escapeRegexChars(ESCAPE_CHARACTER + ESCAPE_CHARACTER), "g");
+		return this.components.map((s : string) => s.replace(regexp_delimiter, this.delimiter).replace(regexp_escape, ESCAPE_CHARACTER)).join(delimiter);
     }
 
     /** 
@@ -37,37 +50,50 @@ export class Name {
      * Machine-readable means that from a data string, a Name can be parsed back in
      * The special characters in the data string are the default characters
      */
+	// @methodtype conversion-method
     public asDataString(): string {
-        throw new Error("needs implementation or deletion");
+		if (this.delimiter !== DEFAULT_DELIMITER) {
+			const regexp_old_delimiter = new RegExp(Name.escapeRegexChars(ESCAPE_CHARACTER + this.delimiter), "g");
+			const regexp_new_delimiter = new RegExp(Name.escapeRegexChars(DEFAULT_DELIMITER), "g");
+			return this.components.map((s) => s.replace(regexp_old_delimiter, this.delimiter).replace(regexp_new_delimiter, ESCAPE_CHARACTER + DEFAULT_DELIMITER)).join(DEFAULT_DELIMITER);
+		} else {
+			return this.components.join(DEFAULT_DELIMITER);
+		}
     }
 
     /** Returns properly masked component string */
+	// @methodtype get-method
     public getComponent(i: number): string {
-        throw new Error("needs implementation or deletion");
+		return this.components[i];
     }
 
     /** Expects that new Name component c is properly masked */
+	// @methodtype set-method
     public setComponent(i: number, c: string): void {
-        throw new Error("needs implementation or deletion");
+		this.components[i] = c;
     }
 
      /** Returns number of components in Name instance */
+	// @methodtype get-method
      public getNoComponents(): number {
-        throw new Error("needs implementation or deletion");
+		 return this.components.length;
     }
 
     /** Expects that new Name component c is properly masked */
+	// @methodtype command-method
     public insert(i: number, c: string): void {
-        throw new Error("needs implementation or deletion");
+		this.components.splice(i, 0, c);
     }
 
     /** Expects that new Name component c is properly masked */
+	// @methodtype command-method
     public append(c: string): void {
-        throw new Error("needs implementation or deletion");
+		this.components.push(c);
     }
 
+	// @methodtype command-method
     public remove(i: number): void {
-        throw new Error("needs implementation or deletion");
+		this.components.splice(i, 1);
     }
 
 }
