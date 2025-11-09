165457bcecb9545b132d0b3715f6d1b70e10be00
maybe-j-fischer <10869729+maybe-j-fischer@users.noreply.github.com>
2025-11-09 22:12:18 +0100

    added homework B02

--- src/adap-b02/names/StringArrayName.ts
+++ src/adap-b02/names/StringArrayName.ts
@@ -7,51 +7,71 @@ export class StringArrayName implements Name {
     protected components: string[] = [];
 
     constructor(source: string[], delimiter?: string) {
-        throw new Error("needs implementation or deletion");
+		this.components = source;
+
+		if (delimiter != undefined) {
+			this.delimiter = delimiter;
+		}
     }
 
+	/** Escapes any character that has a special meaning in regex */
+	private static escapeRegexChars(s: string): string {
+		return s.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+	}
+
     public asString(delimiter: string = this.delimiter): string {
-        throw new Error("needs implementation or deletion");
+		const regexp_delimiter = new RegExp(StringArrayName.escapeRegexChars(ESCAPE_CHARACTER + this.delimiter), "g");
+		const regexp_escape = new RegExp(StringArrayName.escapeRegexChars(ESCAPE_CHARACTER + ESCAPE_CHARACTER), "g");
+		return this.components.map((s : string) => s.replace(regexp_delimiter, this.delimiter).replace(regexp_escape, ESCAPE_CHARACTER)).join(delimiter);
     }
 
     public asDataString(): string {
-        throw new Error("needs implementation or deletion");
+		if (this.delimiter !== DEFAULT_DELIMITER) {
+			const regexp_old_delimiter = new RegExp(StringArrayName.escapeRegexChars(ESCAPE_CHARACTER + this.delimiter), "g");
+			const regexp_new_delimiter = new RegExp(StringArrayName.escapeRegexChars(DEFAULT_DELIMITER), "g");
+			return this.components.map((s) => s.replace(regexp_old_delimiter, this.delimiter).replace(regexp_new_delimiter, ESCAPE_CHARACTER + DEFAULT_DELIMITER)).join(DEFAULT_DELIMITER);
+		} else {
+			return this.components.join(DEFAULT_DELIMITER);
+		}
     }
 
     public getDelimiterCharacter(): string {
-        throw new Error("needs implementation or deletion");
+		return this.delimiter;
     }
 
     public isEmpty(): boolean {
-        throw new Error("needs implementation or deletion");
+		return this.components.length === 0;
     }
 
     public getNoComponents(): number {
-        throw new Error("needs implementation or deletion");
+		return this.components.length;
     }
 
     public getComponent(i: number): string {
-        throw new Error("needs implementation or deletion");
+		return this.components[i];
     }
 
     public setComponent(i: number, c: string): void {
-        throw new Error("needs implementation or deletion");
+		this.components[i] = c;
     }
 
     public insert(i: number, c: string): void {
-        throw new Error("needs implementation or deletion");
+		this.components.splice(i, 0, c);
     }
 
     public append(c: string): void {
-        throw new Error("needs implementation or deletion");
+		this.components.push(c);
     }
 
     public remove(i: number): void {
-        throw new Error("needs implementation or deletion");
+		this.components.splice(i, 1);
     }
 
     public concat(other: Name): void {
-        throw new Error("needs implementation or deletion");
+		/* TODO what should happen if the delimiters differ? */
+		for (let i = 0; i < other.getNoComponents(); ++i) {
+			this.append(other.getComponent(i));
+		}
     }
 
 }
--- src/adap-b02/names/StringName.ts
+++ src/adap-b02/names/StringName.ts
@@ -8,51 +8,91 @@ export class StringName implements Name {
     protected noComponents: number = 0;
 
     constructor(source: string, delimiter?: string) {
-        throw new Error("needs implementation or deletion");
+		this.name = source;
+
+		if (delimiter != undefined) {
+			this.delimiter = delimiter;
+		}
+
+		this.noComponents = this.getComponents().length;
     }
 
+	/** Escapes any character that has a special meaning in regex */
+	private static escapeRegexChars(s: string): string {
+		return s.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+	}
+
+	private getComponents() : string[] {
+		const delimiter_regex = new RegExp("(?<!\\\\(?:\\\\{2})*)" + StringName.escapeRegexChars(this.delimiter), "g");
+		return this.name.split(delimiter_regex);
+	}
+
     public asString(delimiter: string = this.delimiter): string {
-        throw new Error("needs implementation or deletion");
+		const components = this.getComponents();
+		const regexp_delimiter = new RegExp(StringName.escapeRegexChars(ESCAPE_CHARACTER + this.delimiter), "g");
+		const regexp_escape = new RegExp(StringName.escapeRegexChars(ESCAPE_CHARACTER + ESCAPE_CHARACTER), "g");
+		return components.map((s : string) => s.replace(regexp_delimiter, this.delimiter).replace(regexp_escape, ESCAPE_CHARACTER)).join(delimiter);
     }
 
     public asDataString(): string {
-        throw new Error("needs implementation or deletion");
+		if (this.delimiter !== DEFAULT_DELIMITER) {
+			const components = this.getComponents();
+			const regexp_old_delimiter = new RegExp(StringName.escapeRegexChars(ESCAPE_CHARACTER + this.delimiter), "g");
+			const regexp_new_delimiter = new RegExp(StringName.escapeRegexChars(DEFAULT_DELIMITER), "g");
+			return components.map((s) => s.replace(regexp_old_delimiter, this.delimiter).replace(regexp_new_delimiter, ESCAPE_CHARACTER + DEFAULT_DELIMITER)).join(DEFAULT_DELIMITER);
+		} else {
+			return this.name;
+		}
     }
 
     public getDelimiterCharacter(): string {
-        throw new Error("needs implementation or deletion");
+		return this.delimiter;
     }
 
     public isEmpty(): boolean {
-        throw new Error("needs implementation or deletion");
+		return this.noComponents == 0;
     }
 
     public getNoComponents(): number {
-        throw new Error("needs implementation or deletion");
+		return this.noComponents;
     }
 
     public getComponent(x: number): string {
-        throw new Error("needs implementation or deletion");
+		return this.getComponents()[x];
     }
 
     public setComponent(n: number, c: string): void {
-        throw new Error("needs implementation or deletion");
+		const new_components = this.getComponents();
+		new_components[n] = c;
+		this.name = new_components.join(this.delimiter);
     }
 
     public insert(n: number, c: string): void {
-        throw new Error("needs implementation or deletion");
+		const new_components = this.getComponents();
+		new_components.splice(n, 0, c);
+		this.name = new_components.join(this.delimiter);
+		this.noComponents += 1;
     }
 
     public append(c: string): void {
-        throw new Error("needs implementation or deletion");
+		const new_components = this.getComponents();
+		new_components.push(c);
+		this.name = new_components.join(this.delimiter);
+		this.noComponents += 1;
     }
 
     public remove(n: number): void {
-        throw new Error("needs implementation or deletion");
+		const new_components = this.getComponents();
+		new_components.splice(n, 1);
+		this.name = new_components.join(this.delimiter);
+		this.noComponents -= 1;
     }
 
     public concat(other: Name): void {
-        throw new Error("needs implementation or deletion");
+		/* TODO what should happen if the delimiters differ? */
+		for (let i = 0; i < other.getNoComponents(); ++i) {
+			this.append(other.getComponent(i));
+		}
     }
 
 }
